369eb0669721ec5cd93caacba60e070bfb2a1db0
ryannarnoldd <ryannarnoldd@gmail.com>
2021-09-23 15:21:05 -0400

    Added Express with Node and nodemon.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express') // can include express because of the node module // common.js system (module)
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app
+.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+.get('/newpaltz', (req, res) => {
+  res.send('Hello New Paltz')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

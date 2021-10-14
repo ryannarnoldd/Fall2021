cbcb8041389c469134dc085609d054bde8523f97
ryannarnoldd <ryannarnoldd@gmail.com>
2021-10-14 15:15:33 -0400

    Feed and display posts

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -3,12 +3,28 @@
       <h1 class="title">
           Feed Page
       </h1>
+      <div class="columns">
+        <div class="column is-one-third is-offset-one-third">
+
+          <div class="post" v-for="p in posts" :key="p.src">
+            <Post :post="p"/>
+          </div>
+              
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import Post from '../components/Post.vue';
+import session from "../services/session";
+import { GetWall } from "../services/posts";
+
 export default {
-    
+  components: { Post },
+    data: ()=> ({
+      posts: GetWall(session.user.handle)
+    })
 }
 </script>
 

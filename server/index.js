7183d75fd6d3cfaf68d71404756aac7ee44a88b4
ryannarnoldd <ryannarnoldd@gmail.com>
2021-10-25 14:40:08 -0400

    using .env variables

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express'); // can include express because of the node module // common.js system (module)
 const path = require('path')
+require('dotenv').config()
+
+console.log(`The best class at New Paltz is ${process.env.BEST_CLASS}`)
 
 const usersController = require('./controllers/users');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app
     .use('/', express.static(path.join(__dirname, '../docs')))
@@ -15,5 +19,3 @@ app
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-// 'vue ui' 

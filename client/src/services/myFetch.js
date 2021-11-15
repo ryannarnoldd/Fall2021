0f7dbf121a827d582da33bd2de8ba7b833331df1
ryannarnoldd <ryannarnoldd@gmail.com>
2021-11-15 14:19:24 -0500

    env production changes

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -1,4 +1,4 @@
-const API_ROOT = 'localhost:3100/';
+const API_ROOT = process.env.APP_VUE_API_ROOT ?? 'http://localhost:3100/';
 
 export function api(url) {
     return fetch(API_ROOT + url).then(x=> x.json());

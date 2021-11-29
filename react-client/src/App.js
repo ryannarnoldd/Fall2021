8a5a409a5a3fde03b9f942f051e336a73d23f6de
ryannarnoldd <ryannarnoldd@gmail.com>
2021-11-29 15:06:14 -0500

    Added Bulma to React

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <section className="hero is-medium is-link">
+        <div className="hero-body">
+          <p className="title">
+            Medium hero
+          </p>
+          <p className="subtitle">
+            Medium subtitle
+          </p>
+        </div>
+      </section>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
